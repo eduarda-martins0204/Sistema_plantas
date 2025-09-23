56ec07ed5b4625a085f8073f1f85deadb8a3dffa
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 20:32:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
-
-// Linhas para exibir todos os erros (ADICIONE ESTAS DUAS LINHAS)
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-include __DIR__ . "/generic/Autoload.php";
+include "generic/Autoload.php";
 
 use generic\Controller;
 
-if (isset($_GET["param"])){
-    $controller = new Controller ();
+if (isset($_GET["param"])) {
+    $controller = new Controller();
     $controller->verificarChamadas($_GET["param"]);
 }
+
