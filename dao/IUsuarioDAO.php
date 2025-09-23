9f68e57fca96c2773f5a32e490e2e1598f0fc0bd
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 20:27:39 -0300

    Update IUsuarioDAO.php

--- dao/IUsuarioDAO.php
+++ dao/IUsuarioDAO.php
@@ -3,7 +3,10 @@ namespace dao;
 
 interface IUsuarioDAO {
     public function listar();
+    public function listarId($id);
     public function inserir($nome, $email);
     public function alterar($id, $nome, $email);
     public function excluir($id);
 }
+
+
