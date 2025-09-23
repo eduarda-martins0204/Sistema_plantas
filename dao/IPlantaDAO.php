3f8b3125eaebff4611cdd43c5d1fb49f0efe1fcd
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 20:27:28 -0300

    Update IPlantaDAO.php

--- dao/IPlantaDAO.php
+++ dao/IPlantaDAO.php
@@ -3,7 +3,10 @@ namespace dao;
 
 interface IPlantaDAO {
     public function listar();
+    public function listarId($id);
     public function inserir($nome_cientifico, $nome_popular);
     public function alterar($id, $nome_cientifico, $nome_popular);
     public function excluir($id);
 }
+
+
