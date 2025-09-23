faf4e20d0a1cbc18b52e4ee59543aea90e2f9c9f
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 20:27:16 -0300

    Update ICuidadoDAO.php

--- dao/ICuidadoDAO.php
+++ dao/ICuidadoDAO.php
@@ -3,7 +3,10 @@ namespace dao;
 
 interface ICuidadoDAO {
     public function listar();
+    public function listarId($id);
     public function inserir($usuario_id, $planta_id, $tipo_cuidado);
     public function alterar($id, $usuario_id, $planta_id, $tipo_cuidado);
     public function excluir($id);
 }
+
+
