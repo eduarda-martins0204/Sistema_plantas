470e863d907efbcfbcf8d57fbeffbb8a71ffba63
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 03:35:56 -0300

    Update PlantaTemp.php

--- template/PlantaTemp.php
+++ template/PlantaTemp.php
@@ -5,14 +5,16 @@ use template\ITemplate;
 
 class PlantaTemp implements ITemplate {
     public function cabecalho() {
-        // Implementação específica para PlantaTemp
     }
 
     public function rodape() {
-        // Implementação específica para PlantaTemp
     }
 
     public function layout($caminho, $parametro = null) {
-        // Implementação específica para PlantaTemp
+        if ($parametro) {
+            extract(['resultado' => $parametro]);
+        }
+
+        require_once($caminho);
     }
 }
