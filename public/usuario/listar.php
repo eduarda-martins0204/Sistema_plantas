a29f89419bfdefef49d09d0554bfa9eab108c110
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 20:30:26 -0300

    Update listar.php

--- public/usuario/listar.php
+++ public/usuario/listar.php
@@ -1,32 +1,18 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <title>Lista de Cuidados</title>
-</head>
-<body>
-    <h1>Lista de Cuidados</h1>
+<a href="/sistemaplantas/usuario/formulario">Cadastrar</a>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Ações</th>
+    </tr>
+    <?php foreach ($parametro as $p) { ?>
+    <tr>
+        <td><?= $usuario["id"] ?></td>
+        <td><?= $usuario["nome"] ?></td>
+        <td><?= $usuario["email"] ?></td>
+        <td><a href="/sistemaplantas/usuario/formularioalerar?id=<?= $p["id"] ?>">Alterar</a></td>
+    </tr>
+    <?php } ?>
+</table>
 
-    <table border="1">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Usuário ID</th>
-                <th>Planta ID</th>
-                <th>Tipo de Cuidado</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($resultado as $cuidado): ?>
-            <tr>
-                <td><?php echo $cuidado['id']; ?></td>
-                <td><?php echo $cuidado['usuario_id']; ?></td>
-                <td><?php echo $cuidado['planta_id']; ?></td>
-                <td><?php echo $cuidado['tipo_cuidado']; ?></td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-
-</body>
-</html>
