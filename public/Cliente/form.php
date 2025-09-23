862032bf9d3a0cd77261127cbd133ab7f11bc4cf
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-22 22:49:02 -0300

    Update form.php

---
+++ public/Cliente/form.php
@@ -0,0 +1,6 @@
+<h1>Cadastrar Cliente</h1>
+<form method="post" action="salvar.php">
+    Nome: <input type="text" name="nome"><br>
+    Email: <input type="email" name="email"><br>
+    <button type="submit">Salvar</button>
+</form>
