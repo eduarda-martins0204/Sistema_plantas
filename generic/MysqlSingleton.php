6fab3f3e0b0be912474bf3a3b46af046f5f31fd7
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 02:22:56 -0300

    Update MysqlSingleton.php

--- generic/MysqlSingleton.php
+++ generic/MysqlSingleton.php
@@ -1,31 +1,44 @@
 <?php
-class MysqlSingleton{
-    private $usuario = "root";
-    private $senha = "";
+namespace generic;
 
+use PDO;
+use Exception;
 
-    private function __construct(){
-        if($this->conexao == null){
-            $this->conexao = new PDO($this->dsn, $this->usuario, $this->senha);
+class MysqlSingleton {
+    private static $instance; 
+    protected $banco;
+
+    private function __construct() {
+        try {
+            $dsn = "mysql:host=localhost;dbname=sistemaplantas";
+            $usuario = "root";
+            $senha = "";
+
+            $this->banco = new PDO($dsn, $usuario, $senha);
+            $this->banco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->banco->exec("set names utf8");
+        } catch(PDOException $e) {
+            throw new Exception("Erro de Conexão com o Banco de Dados: " . $e->getMessage());
         }
     }
 
-    public static function getInstance(){
-        if(self::$instance == null){
-            self::$instance = new MysqlSingleton();
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new self();
         }
         return self::$instance;
     }
-
-    public function executar($query,$param = array()){
-        if($this->conexao){
-            $sth = $this->conexao->prepare($query);
-            foreach($param as $chave => $valor){
+    
+    public function executar($query, $param = null) {
+        $sth = $this->conexao->prepare($query);
+        
+        if ($param) {
+            foreach ($param as $chave => $valor) {
                 $sth->bindValue($chave, $valor);
             }
-
-            $sth->execute();
-            return $sth->fetchAll(PDO::FETCH_ASSOC);
         }
+        
+        $sth->execute();
+        return $sth->fetchAll(PDO::FETCH_ASSOC);
     }
 }
