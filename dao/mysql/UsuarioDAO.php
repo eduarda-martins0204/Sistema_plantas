4df28bcc80796513c34b9de2ea8bd67ebfc0dc46
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 02:11:03 -0300

    Update UsuarioDAO.php

--- dao/mysql/UsuarioDAO.php
+++ dao/mysql/UsuarioDAO.php
@@ -1,46 +1,44 @@
 <?php
 namespace dao\mysql;
 
-use dai\IUsuarioDAO;
+use dao\IUsuarioDAO;
 use generic\MysqlFactory;
 
-Class UsuarioDAO extends MysqlFactory implements IUsuarioDAO {
-    // Implementação dos métodos da interface IUsuarioDAO
+class UsuarioDAO extends MysqlFactory implements IUsuarioDAO {
 
     public function listar(){
-        $sql = "select id,nome,email from usuarios";
+        $sql = "select id, nome, email from usuarios";
         $retorno = $this->banco->executar($sql);
         return $retorno;
     }
-
-    public function listarID($id){
-        $sql = "select id,nome,email from usuarios where id =: id";
-        $param=[
-            ":id"=>$id
+    
+    public function inserir($nome, $email){
+        $sql = "insert into usuarios (nome, email) values (:nome, :email)";
+        $param = [
+            ":nome" => $nome,
+            ":email" => $email
         ];
-        $retorno = $this->banco->executar($sql);
+        $retorno = $this->banco->executar($sql, $param);
         return $retorno;
     }
 
-    public function inserir($nome,$email){
-        $sql = "insert into usuarios (nome,email,senha) values (:nome,:email)";
-        $param=[
-            ":nome"=>$nome,
-            ":email"=>$email,
+    public function alterar($id, $nome, $email){
+        $sql = "update usuarios set nome = :nome, email = :email where id = :id";
+        $param = [
+            ":id" => $id,
+            ":nome" => $nome,
+            ":email" => $email
         ];
-        $retorno = $this->banco->executar($sql,$param);
+        $retorno = $this->banco->executar($sql, $param);
         return $retorno;
     }
 
-    public function alterar($id,$nome,$email){
-        $sql = "update usuarios set nome=:nome,email=:email where id=:id";
-        $param=[
-            ":id"=>$id,
-            ":nome"=>$nome,
-            ":email"=>$email,
+    public function excluir($id){
+        $sql = "delete from usuarios where id = :id";
+        $param = [
+            ":id" => $id
         ];
-        $retorno = $this->banco->executar($sql,$param);
+        $retorno = $this->banco->executar($sql, $param);
         return $retorno;
     }
 }
-?>
