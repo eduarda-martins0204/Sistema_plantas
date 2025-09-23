0112ac9fe973b5b6c40ffce6729929ecb06dc63d
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 01:43:25 -0300

    Update Controller.php

--- generic/Controller.php
+++ generic/Controller.php
@@ -2,27 +2,34 @@
 namespace generic;
 
 class Controller{
-    private $arrChamadas =[];
+    private $arrChamadas = [];
     public function __construct(){
 
         $this->arrChamadas = [
-            ''=> new Acao('Home', 'index'),
-            "planta/listar"=> new Acao('Planta', 'listar'),
-            "planta/cadastrar"=> new Acao('Planta', 'cadastrar'),
-            "planta/editar"=> new Acao('Planta', 'editar'),
-            "planta/excluir"=> new Acao('Planta', 'excluir'),
+            '' => new Acao('Home', 'index'),
+            "planta/listar" => new Acao('Planta', 'listar'),
+            "planta/cadastrar" => new Acao('Planta', 'cadastrar'),
+            "planta/editar" => new Acao('Planta', 'editar'),
+            "planta/excluir" => new Acao('Planta', 'excluir'),
+
+            "cuidado/listar" => new Acao('Cuidado', 'listar'),
+            "cuidado/inserir" => new Acao('Cuidado', 'inserir'),
+            "cuidado/formulario" => new Acao('Cuidado', 'formulario'),
+            "cuidado/alterarForm" => new Acao('Cuidado', 'alterarForm'),
+            
+            "usuario/listar" => new Acao('Usuario', 'listar'),
+            "usuario/inserir" => new Acao('Usuario', 'inserir'),
+            "usuario/formulario" => new Acao('Usuario', 'formulario'),
+            "usuario/alterarForm" => new Acao('Usuario', 'alterarForm'),
         ];
     }
 
     public function verificarChamadas($rota){
-
         if(isset($this->arrChamadas[$rota])){
-            
             $acao = $this->arrChamadas[$rota];
             $acao->executar();
             return;
-    }
-    echo "Rota nâo existe!";
-        
+        }
+        echo "Rota nâo existe!";
     }
 }
