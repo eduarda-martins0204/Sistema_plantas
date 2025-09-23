53ba9650f4f7c18fd548c903856a4fec6327577b
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 03:18:41 -0300

    Update UsuarioTemp.php

--- template/UsuarioTemp.php
+++ template/UsuarioTemp.php
@@ -5,14 +5,16 @@ use template\ITemplate;
 
 class UsuarioTemp implements ITemplate {
     public function cabecalho() {
-        // Implementação específica para UsuarioTemp
     }
 
     public function rodape() {
-        // Implementação específica para UsuarioTemp
     }
 
     public function layout($caminho, $parametro = null) {
-        // Implementação específica para UsuarioTemp
+        if ($parametro) {
+            extract(['resultado' => $parametro]);
+        }
+        
+        require_once("public/" . $caminho);
     }
 }
