2310e28db60c8bdbc54024013799d2c4b0882ecf
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 01:34:14 -0300

    teste index.php

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
-include "generic/Autoload.php";
+
+// Linhas para exibir todos os erros (ADICIONE ESTAS DUAS LINHAS)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include __DIR__ . "/generic/Autoload.php";
 
 use generic\Controller;
 
 if (isset($_GET["param"])){
-        $controller = new Controller ();
-        $controller->verificarChamadas($_GET["param"]);
+    $controller = new Controller ();
+    $controller->verificarChamadas($_GET["param"]);
 }
