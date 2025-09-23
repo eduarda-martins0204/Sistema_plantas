afbd0e3726e0fd48736f007d0c13ba245eebf466
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 02:18:53 -0300

    Update UsuarioService.php

--- service/UsuarioService.php
+++ service/UsuarioService.php
@@ -3,21 +3,21 @@ namespace service;
 
 use dao\mysql\UsuarioDAO;
 
-Class UsuarioService extends UsuarioDAO{
-    public function listarUsuarios(){
+class UsuarioService extends UsuarioDAO {
+
+    public function listar(){
         return parent::listar();
     }
-
-    public function inserir($nome,$email){
-        return parent::inserir($nome,$email);
+    
+    public function inserir($nome, $email){
+        return parent::inserir($nome, $email);
     }
 
-    public function alterar($id,$nome,$email){
-        return parent::alterar($id,$nome,$email);
+    public function alterar($id, $nome, $email){
+        return parent::alterar($id, $nome, $email);
     }
 
-    public function listarID($id){
-        return parent::listarID($id);
+    public function excluir($id){
+        return parent::excluir($id);
     }
-
 }
