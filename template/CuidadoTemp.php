789ac12384d796dc579a75d590ab2ba3d38bc064
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 03:35:20 -0300

    Update CuidadoTemp.php

--- template/CuidadoTemp.php
+++ template/CuidadoTemp.php
@@ -5,14 +5,16 @@ use template\ITemplate;
 
 class CuidadoTemp implements ITemplate {
     public function cabecalho() {
-        // Implementação específica para CuidadoTemp
     }
 
     public function rodape() {
-        // Implementação específica para CuidadoTemp
     }
 
     public function layout($caminho, $parametro = null) {
-        // Implementação específica para CuidadoTemp
+        if ($parametro) {
+            extract(['resultado' => $parametro]);
+        }
+        
+        require_once($caminho);
     }
 }
