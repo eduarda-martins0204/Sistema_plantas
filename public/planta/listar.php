f95ded858d0b00076146aebc4c8e371a8f6a6a4d
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 02:58:01 -0300

    Update listar.php

--- public/planta/listar.php
+++ public/planta/listar.php
@@ -1,21 +1,30 @@
-<a href="/mvc20251/cliente/formulario">Cadastrar</a>
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Nome</th>
-        <th>Endereco</th>
-        <th>Ações</th>
-    </tr>
-<?php
-foreach( $parametro as $p){
-    ?>
-    <tr>
-        <td><?= $p["id"] ?></td>
-        <td><?= $p["nome"] ?></td>
-        <td><?= $p["endereco"] ?></td>
-        <td><a href="/mvc20251/cliente/formularioalterar?id=<?= $p["id"] ?>">Alterar</a></td>
-    </tr>
-    <?php
-}
-?>
-</table>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Lista de Plantas</title>
+</head>
+<body>
+    <h1>Lista de Plantas</h1>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome Científico</th>
+                <th>Nome Popular</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($resultado as $planta): ?>
+            <tr>
+                <td><?php echo $planta['id']; ?></td>
+                <td><?php echo $planta['nome_cientifico']; ?></td>
+                <td><?php echo $planta['nome_popular']; ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+</body>
+</html>
