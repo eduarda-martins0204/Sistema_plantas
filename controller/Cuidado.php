ebe4ec6a7ed47d4347f8fb7f9962ac0b29001e47
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-23 03:35:01 -0300

    Update Cuidado.php

--- controller/Cuidado.php
+++ controller/Cuidado.php
@@ -1,47 +1,41 @@
-<?php  
-
+<?php
 namespace Controller;
 
 use service\CuidadoService;
 use template\CuidadoTemp;
 use template\ITemplate;
+use generic\Acao;
 
-Class Cuidado{
-    private ITemplate $template;
-    public function __construct(){
-        $this->template = new CuidadoTemp();
-    }
-
-      public function listar(){
-
-        $service = new CuidadoService();
-
-        $resultado = $service->listar();
-
-        $this->template->layout("\\public\\cuidado\\listar.php", $resultado);
-
-    }
-
-    public function inserir() {
-    $usuario_id   = $_POST["usuario_id"];
-    $planta_id    = $_POST["planta_id"];
-    $tipo_cuidado = $_POST["tipo_cuidado"];
-    $service = new CuidadoService();
-    $resultado = $service->inserir($usuario_id, $planta_id, $tipo_cuidado);
-
-    header("location: /mvc20251/cuidado/lista?info=1");
-    }
-
-    public function formulario(){
-        $this->template->layout("\\public\\cuidado\\form.php");
+class Cuidado extends Acao {
+    private $service;
+    
+    public function __construct() {
+        $this->service = new CuidadoService();
     }
     
-    public function alterarForm(){
+    public function listar() {
+        $retorno = $this->service->listar();
+        
+        $this->template->layout("public/cuidado/listar.php", $retorno);
+    }
+    
+    public function inserir() {
+        $usuario_id = $_POST["usuario_id"];
+        $planta_id = $_POST["planta_id"];
+        $tipo_cuidado = $_POST["tipo_cuidado"];
+        $this->service->inserir($usuario_id, $planta_id, $tipo_cuidado);
+        
+        header("location: /sistemaplantas/index.php?param=cuidado/listar");
+    }
+    
+    public function formulario() {
+        $this->template->layout("public/cuidado/form.php");
+    }
+    
+    public function alterarForm() {
         $id = $_GET["id"];
-        $service = new CuidadoService();
-        $resultado = $service->buscarCuidado($id);
+        $retorno = $this->service->buscarCuidado($id);
 
-        $this->template->layout("\\public\\cuidado\\formAlterar.php", $resultado);
+        $this->template->layout("public/cuidado/formAlterar.php", $retorno);
     }
 }
-?>
