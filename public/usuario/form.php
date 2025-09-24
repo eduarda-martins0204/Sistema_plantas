51f681d7a6d1eccc5725b277fd903085cdad45f4
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-24 00:15:34 -0300

    Update form.php

--- public/usuario/form.php
+++ public/usuario/form.php
@@ -1,25 +1,36 @@
-<form method="POST" action="/sistemaplantas/usuario/inserir">
-    <label>Nome: </label>
-    <input type="text" name="nome" 
-        value="<?= ($parametro != null) ? $parametro[0]["nome"] : "" ?>" />
-    <br />
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Usuário</title>
+</head>
+<body>
+    <h1>Sistema Plantas 🌱</h1>
+    <form method="POST" action="/sistemaplantas/index.php?param=usuario/inserir">
+        <label>Nome: </label>
+        <input type="text" name="nome" required
+            value="<?= isset($parametro) ? $parametro[0]["nome"] : "" ?>" />
+        <br />
 
-    <label>Endereco: </label>
-    <input type="text" name="endereco" 
-        value="<?= ($parametro != null) ? $parametro[0]["endereco"] : "" ?>" />
-    <br />
+        <label>E-mail: </label>
+        <input type="email" name="email" required
+            value="<?= isset($parametro) ? $parametro[0]["email"] : "" ?>" />
+        <br />
 
-    <?php
-    if ($parametro != null) {
-    ?>
-        <input type="hidden" name="id" value="<?= $parametro[0]["id"] ?>" />
-    <?php
-    }
-    ?>
+        <label>Senha: </label>
+        <input type="password" name="senha" required />
+        <br />
 
-    <input type="submit" value="enviar" />
-</form>
+        <?php
+        if (isset($parametro) && $parametro != null) {
+        ?>
+            <input type="hidden" name="id" value="<?= $parametro[0]["id"] ?>" />
+        <?php
+        }
+        ?>
 
-
-
-
+        <input type="submit" value="enviar" />
+    </form>
+</body>
+</html>
