dce0680c1853aef6717977301dbc5c014e8b834d
eduarda-martins0204 <145498651+eduarda-martins0204@users.noreply.github.com>
2025-09-24 00:17:24 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
 include  "generic/Autoload.php";
 
-var_dump($_GET['param']);
-exit;
-
-use generic\Controller;
-
-if (isset($_GET["param"])){
-    $controller = new Controller();
-    $controller->verificarChamadas($_GET["param"]);
-}
-
 use generic\Controller;
 
 if (isset($_GET["param"])) {
